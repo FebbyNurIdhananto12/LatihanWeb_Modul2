729119144e88c524f3843999c0e18bfeb9687a46
FebbyNurIdhananto12 <129040396+FebbyNurIdhananto12@users.noreply.github.com>
2023-10-12 09:58:53 +0700

    Add files via upload

---
+++ code_lab2/validasi.js
@@ -0,0 +1,24 @@
+function validateAndSubmit() {
+  var nama = document.getElementById("nama").value;
+  var email = document.getElementById("email").value;
+  var alamat = document.getElementById("alamat").value;
+
+  if (nama === "" || email === "" || alamat === "") {
+    alert("Anda harus mengisi data dengan lengkap !");
+  } else {
+    document.getElementById("registrationForm").submit();
+  }
+}
+
+function validateForm() {
+  var nama = document.getElementById("nama").value;
+  var email = document.getElementById("email").value;
+  var alamat = document.getElementById("alamat").value;
+
+  if (nama === "" || email === "" || alamat === "") {
+    alert("Anda harus mengisi data dengan lengkap !");
+    return false;
+  }
+
+  return true;
+}
